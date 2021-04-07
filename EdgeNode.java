6de29bfaae323c5ab41ccc8314630e02a004e3ec
Shaunak Karuna s3782215 <s3782215@student.rmit.edu.au>
2021-04-07 20:47:05 +1000

    EdgeNode.java and VertexNode.java commenting complete

--- EdgeNode.java
+++ EdgeNode.java
@@ -1,23 +1,40 @@
+/**
+ * This class represents a Node in the EdgeLinkedList object
+ * which will store an edge and the next EdgeNode in the list each
+ */
 public class EdgeNode
 {
-    /** Stored value of edge. */
-    protected Edge edge;
-    /** Reference to next node. */
-    protected EdgeNode nextEdgeNode;
+    //Stored Edge Value
+    private Edge edge;
 
+    //Stored value to the next EdgeNode in the List
+    private EdgeNode nextEdgeNode;
+
+    /**
+     * Constructor for EdgeNode object
+     * @param edge The edge object that will be stored in this node
+     */
     public EdgeNode(Edge edge)
     {
         this.edge = edge;
         nextEdgeNode = null;
     }
 
+    /**
+     * This is a getter method for the edge value stored
+     * @return Edge that is stored
+     */
     public Edge getEdge() { return edge; }
 
-
+    /**
+     * This is a getter method for the next EdgeNode in the list
+     * @return EdgeNode that is the next value in the list
+     */
     public EdgeNode getNext() { return nextEdgeNode; }
 
-    public void setNext(EdgeNode next)
-        {
-            nextEdgeNode = next;
-        }
+    /**
+     * This is a setter for the next value in the list
+     * @param next EdgeNode that is next in the list
+     */
+    public void setNext(EdgeNode next) { nextEdgeNode = next;}
 }
--- VertexNode.java
+++ VertexNode.java
@@ -1,30 +1,40 @@
+/**
+ * This class represents a Node in the VertexLinkedList object
+ * which will store an vertex and the next VertexNode in the list each
+ */
 public class VertexNode
 {
+    //Stored Edge Value
+    private Vertex vertex;
 
-    /** Stored value of vertex. */
-    protected Vertex vertex;
-    /** Reference to next node. */
-    protected VertexNode nextNode;
+    //Stored value to the next EdgeNode in the List
+    private VertexNode nextNode;
 
+    /**
+     * Constructor for VertexNode object
+     * @param vertex The Vertex object that will be stored in this node
+     */
     public VertexNode(Vertex vertex)
     {
         this.vertex = vertex;
         nextNode = null;
     }
 
-    public Vertex getVertex()
-        {
+    /**
+     * This is a getter method for the vertex value stored
+     * @return Vertex that is stored
+     */
+    public Vertex getVertex() { return vertex; }
 
-            return vertex;
-        }
-        public VertexNode getNext()
-        {
+    /**
+     * This is a getter method for the next VertexNode in the list
+     * @return VertexNode that is the next value in the list
+     */
+    public VertexNode getNext() { return nextNode; }
 
-            return nextNode;
-        }
-        public void setNext(VertexNode next)
-        {
-
-            nextNode = next;
-        }
+    /**
+     * This is a setter for the next value in the list
+     * @param next VertexNode that is next in the list
+     */
+    public void setNext(VertexNode next) { nextNode = next; }
 }
